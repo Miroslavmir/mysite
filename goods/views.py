b647ab2bcff1ddbccb3fd9190689c945c8075739
Admin <user.@mail.com>
2024-01-21 13:42:13 +0300

    slug convertor for category

--- goods/views.py
+++ goods/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render
 # Create your views here.
 from goods.models import Products
 
 
-def catalog(request):
-    goods = Products.objects.all
+def catalog(request, category_slug):
+    if category_slug == 'all':
+        goods = Products.objects.all
+
+    else:
+        goods = get_object_or_404(Products.objects.filter(category__slug=category_slug))
+
+
 
 
     context = {
@@ -16,5 +21,10 @@ def catalog(request):
     return render(request, 'goods/catalog.html', context)
 
 
-def product(request):
-    return render(request, 'goods/product.html')
+def product(request, product_slug,):
+    product = Products.objects.get(slug=product_slug)
+
+    context = {
+        'product': product
+    }
+    return render(request, 'goods/product.html', context=context)
